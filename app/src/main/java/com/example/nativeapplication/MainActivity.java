c297f1dbf1ad99b8bd393219e852223920e9419a
swadeshnayak <swadeshnayakfeb@gmail.com>
2023-02-23 15:59:29 +0100

    Home Page, Post your business and Order confirmation page merged with service subcategories

--- app/src/main/java/com/example/nativeapplication/MainActivity.java
+++ app/src/main/java/com/example/nativeapplication/MainActivity.java
@@ -2,12 +2,12 @@ package com.example.nativeapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import android.content.Intent;
-import android.view.View;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-
+import android.widget.ImageButton;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,25 +15,126 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Spinner to handel search bar
+        Spinner spinner = findViewById(R.id.locationSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.location_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
 
-        Button ib = (Button) findViewById(R.id.button2);
-        ib.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String value= "Men's Salon,Women's Salon,SPA & Massage,Facial & Clean-ups";
-                Intent i = new Intent(MainActivity.this, ServicesSubcategories.class);
-                i.putExtra("category", value);
-
-                startActivity(i);
-            }
+        //Functionality: Post Your Business Button Onclick navigate to Post Your Business Screen
+        Button myButton = findViewById(R.id.button5);
+        myButton.setOnClickListener(v -> {
+            Intent i = new Intent(MainActivity.this, PostYourBusiness.class);
+            startActivity(i);
+        });
+        //Set1: Salon Image Button and Button
+        //Functionality: Salon Image Button Onclick navigate to Service Page Screen
+        ImageButton imageButton = findViewById(R.id.imageButton8);
+        imageButton.setOnClickListener(v -> {
+            String value= "Men's Salon,Women's Salon,SPA & Massage,Facial & Clean-ups";
+            Intent i = new Intent(MainActivity.this, ServicesSubcategories.class);
+            i.putExtra("category", value);
+            startActivity(i);
+        });
+        //Functionality: Salon Button Onclick navigate to Service Page Screen
+        Button myButton1 = findViewById(R.id.button10);
+        myButton1.setOnClickListener(v -> {
+            String value= "Men's Salon,Women's Salon,SPA & Massage,Facial & Clean-ups";
+            Intent i = new Intent(MainActivity.this, ServicesSubcategories.class);
+            i.putExtra("category", value);
+            startActivity(i);
         });
 
+        //Set2: Electrician Image Button and Button
+        //Functionality: Electrician Image Button Onclick navigate to Service Page Screen
+        ImageButton imageButton1 = findViewById(R.id.imageButton9);
+        imageButton1.setOnClickListener(v -> {
+            String value= "Appliances,Switch & Socket,Wiring,Light";
+            Intent i = new Intent(MainActivity.this, OrderConfirmation.class);
+            i.putExtra("category", value);
+            startActivity(i);
+        });
+        //Functionality: Electrician Button Onclick navigate to Service Page Screen
+        Button myButton2 = findViewById(R.id.button11);
+        myButton2.setOnClickListener(v -> {
+            String value= "Appliances,Switch & Socket,Wiring,Light";
+            Intent i = new Intent(MainActivity.this, OrderConfirmation.class);
+            i.putExtra("category", value);
+            startActivity(i);
+        });
 
+        //Set3: Plumber Image Button and Button
+        //Functionality: Plumber Image Button Onclick navigate to Service Page Screen
+        ImageButton imageButton2 = findViewById(R.id.imageButton10);
+        imageButton2.setOnClickListener(v -> {
+            String value= "Drainage Pipes,Basin & Toilets,Bath & Water Fittings,Grouting";
+            Intent i = new Intent(MainActivity.this, OrderConfirmation.class);
+            i.putExtra("category", value);
+            startActivity(i);
+        });
+        //Functionality: Plumber Button Onclick navigate to Service Page Screen
+        Button myButton3 = findViewById(R.id.button12);
+        myButton3.setOnClickListener(v -> {
+            String value= "Drainage Pipes,Basin & Toilets,Bath & Water Fittings,Grouting";
+            Intent i = new Intent(MainActivity.this, OrderConfirmation.class);
+            i.putExtra("category", value);
+            startActivity(i);
+        });
 
-
-
-
+        //Set4: Others Image Button and Button
+        //Functionality: Others Image Button Onclick navigate to Service Page Screen
+        ImageButton imageButton3 = findViewById(R.id.imageButton11);
+        imageButton3.setOnClickListener(v -> {
+            String value= "Baby Sitting,Furniture Repair,Drilling,Furniture Assembly";
+            Intent i = new Intent(MainActivity.this, OrderConfirmation.class);
+            i.putExtra("category", value);
+            startActivity(i);
+        });
+        //Functionality: Others Button Onclick navigate to Service Page Screen
+        Button myButton4 = findViewById(R.id.button13);
+        myButton4.setOnClickListener(v -> {
+            String value= "Baby Sitting,Furniture Repair,Drilling,Furniture Assembly";
+            Intent i = new Intent(MainActivity.this, OrderConfirmation.class);
+            i.putExtra("category", value);
+            startActivity(i);
+        });
     }
 
-
 }
+
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import android.os.Bundle;
+//
+//import android.content.Intent;
+//import android.view.View;
+//import android.widget.Button;
+//
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        Button ib = (Button) findViewById(R.id.button2);
+//        ib.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String value= "Men's Salon,Women's Salon,SPA & Massage,Facial & Clean-ups";
+//                Intent i = new Intent(MainActivity.this, ServicesSubcategories.class);
+//                i.putExtra("category", value);
+//
+//                startActivity(i);
+//            }
+//        });
+
+
+//
+//    }
+//
+//
+//}
